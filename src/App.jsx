f96fe2ed4ea03abfc92b32c344779f28b3c6340b
Alim1899 <alimakhchanogli@gmail.com>
2025-09-17 22:36:35 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 style={{ margin: "auto", width: "100%", textAlign: "center" }}>
+      Redberry
+    </h1>
+  );
+}
+
+export default App;
